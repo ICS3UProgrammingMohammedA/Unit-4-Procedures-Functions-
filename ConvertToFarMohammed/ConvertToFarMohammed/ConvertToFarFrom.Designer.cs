f86da730bee7281d44f058ec6bf8f2429b2caaf8
MohammedAlnajeh <43445141+MohammedAlnajeh@users.noreply.github.com>
2018-11-19 10:24:08 -0500

    added convert to farheight

--- ConvertToFarMohammed/ConvertToFarMohammed/ConvertToFarFrom.Designer.cs
+++ ConvertToFarMohammed/ConvertToFarMohammed/ConvertToFarFrom.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtCelsuis = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(231, 240);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(99, 41);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // txtCelsuis
+            // 
+            this.txtCelsuis.Location = new System.Drawing.Point(410, 152);
+            this.txtCelsuis.Name = "txtCelsuis";
+            this.txtCelsuis.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsuis.TabIndex = 1;
             // 
             // frmConvertToFar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(552, 324);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(601, 370);
+            this.Controls.Add(this.txtCelsuis);
+            this.Controls.Add(this.btnCalculate);
             this.Name = "frmConvertToFar";
-            this.Text = "Form1";
+            this.Text = "Convert To Far By Mohammed";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtCelsuis;
     }
 }
 
