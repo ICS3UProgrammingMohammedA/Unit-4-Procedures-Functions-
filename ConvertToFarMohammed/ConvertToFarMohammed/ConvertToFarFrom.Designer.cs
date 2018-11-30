b1034efeeebfd919d80aca81bbb2ab05b1fa71e4
MohammedAlnajeh <43445141+MohammedAlnajeh@users.noreply.github.com>
2018-11-30 10:53:06 -0500

    completed convert to far

--- ConvertToFarMohammed/ConvertToFarMohammed/ConvertToFarFrom.Designer.cs
+++ ConvertToFarMohammed/ConvertToFarMohammed/ConvertToFarFrom.Designer.cs
@@ -31,6 +31,7 @@
             this.btnConvert = new System.Windows.Forms.Button();
             this.txtCelsuis = new System.Windows.Forms.TextBox();
             this.lblEnter = new System.Windows.Forms.Label();
+            this.lblAns = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnConvert
@@ -42,6 +43,7 @@
             this.btnConvert.TabIndex = 0;
             this.btnConvert.Text = "Convert";
             this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
             // 
             // txtCelsuis
             // 
@@ -60,18 +62,28 @@
             this.lblEnter.TabIndex = 2;
             this.lblEnter.Text = "Enter The tempreature in Degrees celsius";
             // 
+            // lblAns
+            // 
+            this.lblAns.AutoSize = true;
+            this.lblAns.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAns.Location = new System.Drawing.Point(49, 141);
+            this.lblAns.Name = "lblAns";
+            this.lblAns.Size = new System.Drawing.Size(18, 20);
+            this.lblAns.TabIndex = 3;
+            this.lblAns.Text = "?";
+            // 
             // frmConvertToFar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(601, 233);
+            this.Controls.Add(this.lblAns);
             this.Controls.Add(this.lblEnter);
             this.Controls.Add(this.txtCelsuis);
             this.Controls.Add(this.btnConvert);
             this.Name = "frmConvertToFar";
             this.Text = "Convert To Far By Mohammed";
-            this.Load += new System.EventHandler(this.frmConvertToFar_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +94,7 @@
         private System.Windows.Forms.Button btnConvert;
         private System.Windows.Forms.TextBox txtCelsuis;
         private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.Label lblAns;
     }
 }
 
--- ConvertToFarMohammed/ConvertToFarMohammed/ConvertToFarFrom.cs
+++ ConvertToFarMohammed/ConvertToFarMohammed/ConvertToFarFrom.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Mohammed Alnajeh
+ * Created on: 2018-11-30
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Area Procedure
+ * This program...
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,10 +25,34 @@ namespace ConvertToFarMohammed
             InitializeComponent();
         }
 
-        public void ConvertToFar(int tf, int tc)
+        public double ConvertToFar( double Celsius)
         {
-            int degrees;
+            //declare  variables
+            double far = 0;
+
+            //convert from farinhieght to celsius
+            far = Celsius * (9) / (double)(5) + 32;
+
+            return far;
             
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //declare local variaalbles
+            double usercelsius;
+
+            //convert from string to a double
+            usercelsius = double.Parse(txtCelsuis.Text);
+
+            //calling function
+            usercelsius = ConvertToFar(usercelsius);
+            
+            //convert to string
+            lblAns.Text = string.Format("{0:0.00}", usercelsius) + "far";
+
+
+
         }
     }
 }
